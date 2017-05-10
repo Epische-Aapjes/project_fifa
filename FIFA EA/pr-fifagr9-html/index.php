d05fed0d6c1b4a9d764fb87ad6604711e442305a
jerrydemiranda <D201662@edu.rocwb.nl>
2017-05-10 13:10:05 +0200

    made second slide

--- FIFA EA/pr-fifagr9-html/index.php
+++ FIFA EA/pr-fifagr9-html/index.php
@@ -162,22 +162,68 @@
   <div class="numbertext">2 / 4</div>
   <!-- ==================slide 3 form======================== -->  
   
-  <form action="">
+  <form class="formall" action="">
   <div class="champ">
 <input id="champ" type="text" name=""> 
 <h5 id="champw">kampioen</h5>
   </div>
-<input id="" type="text" name="">  
-<input id="" type="text" name=""> 
-<input id="" type="text" name=""> 
-<input id="" type="text" name=""> 
-<input id="" type="text" name=""> 
-<input id="" type="text" name=""> 
-<input id="" type="text" name=""> 
-<input id="" type="text" name=""> 
-<input id="" type="text" name=""> 
-<input id="" type="text" name=""> 	
+  <ul  class="overall" 	>
 
+ <li>
+     <ul  class="row1" >
+       <li><input id="" type="text" name=""></li> 
+        <li><img  id="picvs"  src="img/storm.png" alt=""></li>
+       <li><input id="" type="text" name=""></li>
+             <li> <img id="paddingtool" src="img/paddingtool.png" alt=""> </li>
+       <li><input  type="text" name=""></li>
+        <li><img  id="picvs"  src="img/storm.png" alt=""></li>
+       <li><input id="" type="text" name=""></li>
+    </ul>
+ </li>
+
+ <li>
+    <ul  class="row2" >
+ 	  <li><input id="" type="text" name=""></li> 
+ 	   <li><img  id="picvs"  src="img/storm.png" alt=""></li>
+      <li><input id="" type="text" name=""></li>
+    </ul>
+ </li>
+    
+    <li>
+      <ul class="singles">
+    <li><input   type="text" name=""></li>
+       
+       <li><img id="vspic" src="img/VS.png"></li>
+
+    <li><input   type="text" name=""></li> 
+      </ul>
+    </li>
+
+ <li>
+
+    <ul  class="row3">
+ 	  <li><input id="" type="text" name=""></li>
+ 	   <li><img  id="picvs"  src="img/storm.png" alt=""></li> 
+      <li><input id="" type="text" name=""></li>
+    </ul>
+ </li>
+
+  <li>
+     <ul  class="row4">
+       <li><input id="" type="text" name=""></li> 
+       <li><img  id="picvs"  src="img/storm.png" alt=""></li>
+       <li><input id="" type="text" name=""></li>
+          <li> <img id="paddingtool" src="img/paddingtool.png" alt=""> </li>
+       <li><input  type="text" name=""></li>
+        <li><img  id="picvs"  src="img/storm.png" alt=""></li>
+       <li><input id="" type="text" name=""></li>
+    </ul>
+ </li>
+
+
+
+
+</ul>
 
 
 
