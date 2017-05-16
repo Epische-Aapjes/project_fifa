a45867a8e4c45c80b4101de3e327301c8cfa4912
EXtermin <Damian.meijer@gmail.com>
2017-05-16 15:16:46 +0200

    Fixed login.php

--- FIFA EA/pr-fifagr9-html/app/login.php
+++ FIFA EA/pr-fifagr9-html/app/login.php
@@ -5,17 +5,58 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <title>Login</title>
 </head>
 <body>
- <p>Please Login!</p>
-
-<form action="">
 
 
+ <div class="row">
+     <div class="col-sm-9">
+         <div class="row">
+             <div class="col-8 col-sm-6">
+             </div>
+             <div class="col-4 col-sm-6">
+                 <h1>Login</h1>
+                    <form action="loginchecker.php" method="post" class="form-inline">
 
-</form>
 
+                        <label class="sr-only" for="inlineFormInput">Username</label>
+                        <input type="text" class="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput">
+
+                        <label class="sr-only" for="inlineFormInput">Password</label>
+                        <div class="input-group mb-2 mr-sm-2 mb-sm-0">
+
+                            <div class="input-group-addon">@</div>
+                            <input type="password" class="form-control" id="inlineFormInputGroup">
+                        </div>
+                        <div class="form-check mb-2 mr-sm-2 mb-sm-0">
+                            <label class="form-check-label">
+                                <input class="form-check-input" type="checkbox"> Remember me
+                            </label>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <?php
+                        error_reporting(0);
+
+                        if ($_GET['message'])
+                        {
+                            $message = $_GET['message'];
+                            echo "<div class='alert alert-danger' role='alert'>";
+                            echo "  <strong>Oh snap!</strong>";
+                            echo "   <a href='login.php' class='alert-link'>$message</a>";
+                            echo "   en probeer het opnieuw.";
+                            echo "</div>";
+                        }
+                        else{
+
+                        }
+                        ?>
+                 </form>
+             </div>
+         </div>
+     </div>
+ </div>
 
 
 
--- FIFA EA/pr-fifagr9-html/app/loginchecker.php
+++ FIFA EA/pr-fifagr9-html/app/loginchecker.php
@@ -1,12 +1,19 @@
 <?php
-$username = $_POST['username'];
-$password = $_POST['password'];
+
+require ('database.php');
+$username = $_POST['inlineFormInput'];
+$password = $_POST['inlineFormInputGroup'];
 
 if ($username != '')
 {
     if ($password != '')
     {
-
+            $admin = 0;
+            $sql ="INSERT INTO tbl_users(Username, Password, IsAdmin) VALUES ( '$username' ,'$password' , '$admin')";
+            $sth = $databse->prepare('$sql');
+            $sth->excute();
+        $message='U bent nu ingelogd';
+        header("location:../index.php?message=$message");
     }
     else
     {
@@ -17,6 +24,7 @@ if ($username != '')
 }
 else{
     $message = 'Vul alstublief een gebruikersnaam in';
+
     header("location:../app/login.php?message=$message");
 }
 
