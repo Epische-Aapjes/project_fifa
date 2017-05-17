08e1a30bf6eb179f09117f9cb9ad16419b9c8def
jerrydemiranda <D201662@edu.rocwb.nl>
2017-05-17 15:53:31 +0200

    Auto stash before revert of "More improvements on login"

--- FIFA EA/pr-fifagr9-html/app/loginchecker.php
+++ FIFA EA/pr-fifagr9-html/app/loginchecker.php
@@ -1,32 +1,31 @@
 <?php
 
 require ('database.php');
-$username = $_POST['username'];
-$password = $_POST['password'];
-var_dump($username);
-var_dump($password);
+$username = $_POST['inlineFormInput'];
+$password = $_POST['inlineFormInputGroup'];
+
 if ($username != '')
 {
     if ($password != '')
     {
             $admin = 0;
-            $stmt = $databse->prepare("INSERT INTO tbl_users(Username, Password, IsAdmin) VALUES ( :username, :password, :admin)");
-
-            $stmt->excute();
+            $sql ="INSERT INTO tbl_users(Username, Password, IsAdmin) VALUES ( '$username' ,'$password' , '$admin')";
+            $sth = $databse->prepare('$sql');
+            $sth->excute();
         $message='U bent nu ingelogd';
-        //header("location:../index.php?message=$message");
+        header("location:../index.php?message=$message");
     }
     else
     {
         $message = 'Vul alstublief een wachtwoord in';
-        //header("location:../app/login.php?message=$message");
+        header("location:../app/login.php?message=$message");
     }
 
 }
 else{
     $message = 'Vul alstublief een gebruikersnaam in';
 
-    //header("location:../app/login.php?message=$message");
+    header("location:../app/login.php?message=$message");
 }
 
 
