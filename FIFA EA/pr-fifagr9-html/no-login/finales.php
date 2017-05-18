c494d8a2939ac0977a84b692a3918f0eba5d2bd0
jerrydemiranda <D201662@edu.rocwb.nl>
2017-05-18 10:21:36 +0200

    home

---
+++ FIFA EA/pr-fifagr9-html/no-login/finales.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="header.css">
+</head>
+<body>
+
+<div class="header">
+  <h1>FIFA-BETS</h1>
+  <ul>
+    <li><a href="./resultaten.php">Resultaten</a></li>
+    <li style="background-color: #787878"><a href="./finales.php">Finales</a></li>
+    
+  </ul>
+</div> 
+<div class="main">
+<form class="formall" action="">
+  <div class="champ">
+<input id="champ" type="text" name=""> 
+<h5 id="champw">kampioen</h5>
+  </div>
+  <ul  class="overall" 	>
+
+ <li>
+     <ul  class="row1" >
+       <li><input id="" type="text" name=""></li> 
+        <li><img  id="picvs"  src="img/storm.png" alt=""></li>
+       <li><input id="" type="text" name=""></li>
+             <li> <img id="paddingtool" src="img/paddingtool.png" alt=""> </li>
+       <li><input  type="text" name=""></li>
+        <li><img  id="picvs"  src="img/storm.png" alt=""></li>
+       <li><input id="" type="text" name=""></li>
+    </ul>
+ </li>
+
+ <li>
+    <ul  class="row2" >
+ 	  <li><input id="" type="text" name=""></li> 
+ 	   <li><img  id="picvs"  src="img/storm.png" alt=""></li>
+      <li><input id="" type="text" name=""></li>
+    </ul>
+ </li>
+    
+    <li>
+      <ul class="singles">
+    <li><input   type="text" name=""></li>
+       
+       <li><img id="vspic" src="img/VS.png"></li>
+
+    <li><input   type="text" name=""></li> 
+      </ul>
+    </li>
+
+ <li>
+
+    <ul  class="row3">
+ 	  <li><input id="" type="text" name=""></li>
+ 	   <li><img  id="picvs"  src="img/storm.png" alt=""></li> 
+      <li><input id="" type="text" name=""></li>
+    </ul>
+ </li>
+
+  <li>
+     <ul  class="row4">
+       <li><input id="" type="text" name=""></li> 
+       <li><img  id="picvs"  src="img/storm.png" alt=""></li>
+       <li><input id="" type="text" name=""></li>
+          <li> <img id="paddingtool" src="img/paddingtool.png" alt=""> </li>
+       <li><input  type="text" name=""></li>
+        <li><img  id="picvs"  src="img/storm.png" alt=""></li>
+       <li><input id="" type="text" name=""></li>
+    </ul>
+ </li>
+
+
+
+
+</ul>
+
+
+
+
+
+  </form>
+</div>
+
+
+	
+
+ 
+  
+</div>
+</body>
+</html>
---
+++ FIFA EA/pr-fifagr9-html/no-login/resultaten.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="header.css">
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	
+	<div class="header">
+  <h1>FIFA-BETS</h1>
+  <ul>
+    <li style="background-color: #787878"><a href="./resultaten.php">Resultaten</a></li>
+    <li><a href="./finales.php">Finales</a></li>
+    
+  </ul>
+</div> 
+<div class="main">
+
+
+
+
+	<form action="">
+	
+	<div class="lislide1f">
+		
+			<ul class="ulslide1a">
+				<li><input   type="text" name=""></li>
+				<li><input   type="text" name=""></li>
+				<li><input   type="text" name=""></li>
+				<li><input   type="text" name=""></li>
+				<li><input   type="text" name=""></li>
+				<li><input   type="text" name=""></li>
+				<li><input   type="text" name=""></li>
+				<li><input   type="text" name=""></li>
+			</ul>
+	
+		    
+
+				
+			<ul class="ulslide1b">
+				<li><input   type="text" name=""></li>
+				<li><input   type="text" name=""></li>
+				<li><input   type="text" name=""></li>
+				<li><input   type="text" name=""></li>
+				<li><input   type="text" name=""></li>
+				<li><input   type="text" name=""></li>
+				<li><input   type="text" name=""></li>
+				<li><input   type="text" name=""></li>
+			</ul>
+
+		
+
+		
+			<ul class="ulslide1c">
+				<li><input   type="text" name=""></li>
+				<li><input   type="text" name=""></li>
+				<li><input   type="text" name=""></li>
+				<li><input   type="text" name=""></li>
+				<li><input   type="text" name=""></li>
+				<li><input   type="text" name=""></li>
+				<li><input   type="text" name=""></li>
+				<li><input   type="text" name=""></li>
+			</ul>
+
+		
+</div>
+
+</div>
+
+
+	
+
+
+
+
+</form>
+</body>
+</html>
