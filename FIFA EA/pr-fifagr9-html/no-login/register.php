2f7400b024736c28b4cebed313844c5ed7d4432f
EXtermin <Damian.meijer@gmail.com>
2017-05-31 16:19:56 +0200

    Regster + Bugs

--- FIFA EA/pr-fifagr9-html/no-login/register.php
+++ FIFA EA/pr-fifagr9-html/no-login/register.php
@@ -9,10 +9,13 @@
  <div class="main" >
 	<h2>hier kunt u registreren voor fifa bets</h2>
 
-	<form action="">
-		
-
+	<form action="../app/registerChecker.php" method="post">
+        <label for="username">Gebruikersnaam:</label>
+        <input type="text" name="username">
+        <label for="password">Wachtwoord:</label>
+        <input type="password" name="password">
 	</form>
+     <a href="../index.php">Already registered? Click here!</a>
 </div>
 </body>
 </html>
